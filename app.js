81de26d8e0af8ce1366ee9a078d9c5355e502fc7
Sushant Singh <sushanth.singh@webileapps.com>
2023-02-14 20:08:16 +0530

    initial c

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const yaml = require("yamljs");
+const swaggerUi = require("swagger-ui-express");
+
+const app = express();
+
+app.use(express.json());
+
+const swaggerDocument = yaml.load("apidoc.yaml");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.get("/", (_req, res) => {
+  res
+    .status(200)
+    .send(
+      `Hello from Library Management service!. To visit api docs visit <a href="./api-docs/">here</a>`
+    );
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/users", (req, res) => {
+  console.log(req.body);
+  res.status(201).send({
+    id: Math.random() * 1e5,
+    name: req.body.name,
+    email: req.body.email,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    isActive: true,
+    role: "student",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Example app listening on port 3000!");
+});
+// Path: app.js
